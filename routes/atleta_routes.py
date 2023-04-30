2672fe1995e7843cdc2a866f40da7d235ec4e78c
Eduardo Iotti <ddz.iotti@gmail.com>
2023-04-30 12:57:50 -0300

    feat: adicionar instruções openapi

--- routes/atleta_routes.py
+++ routes/atleta_routes.py
@@ -1,11 +1,22 @@
-from flask import Blueprint, jsonify, request
+from flask_openapi3 import Tag, APIBlueprint
+from flask import jsonify, request
 from model import Session
 from model.atleta import Atleta
+from schemas import ListarAtletasSchema, AdicionarAtletasSchema, FormAddAtletaSchema, ListarAtletaIdSchema, DeletarAtletaIdSchema
+from logger import logger
+from schemas.atleta import AtletaPath
 
-atleta_routes = Blueprint('atleta_routes', __name__)
+atletas_tag = Tag(
+    name='Atletas', description='Operações relacionadas a atletas')
 
-@atleta_routes.route('/atletas', methods=['GET'])
+atleta_routes = APIBlueprint('atletas', __name__, abp_tags=[atletas_tag])
+
+
+@atleta_routes.get('/atletas',  responses={"200": ListarAtletasSchema})
 def listar_atletas():
+    """ Retorna uma listagem de atletas cadastrados no banco de dados.
+
+    """
     # listar atletas do banco
     session = Session()
     atletas = session.query(Atleta).all()
@@ -15,14 +26,98 @@ def listar_atletas():
     for atleta in atletas:
         atletas_arr.append(atleta.toDict())
 
+    # retorna erro se o array for vazio:
+    if not atletas_arr:
+        error_msg = "Nenhum Atleta foi  encontrado na base."
+        logger.warning(f"Erro ao listar atletas:', {error_msg}")
+        return {"mesage": error_msg}, 404
+
     return jsonify(atletas_arr)
 
-@atleta_routes.route('/atletas', methods=['POST'])
-def adicionar_atleta():
-    session = Session()
-    atleta = Atleta(request.form['nome'], request.form['sexo'], request.form['idade'], request.form['cidade'], request.form['categoria'])
 
+@atleta_routes.get('/atletas/<int:id>',  responses={"200": ListarAtletaIdSchema})
+def listar_atletas_por_id(path: AtletaPath):
+    """ Retorna atleta específico cadastrados no banco de dados, de acordo com o ID especificado.
+    """
+
+    # listar atleta específico do banco:
+    atleta_id = path.id
+    session = Session()
+    atleta = session.query(Atleta).filter(Atleta.id == atleta_id).first()
+
+    if not atleta:
+        # se o atleta não foi encontrado
+        error_msg = "Atleta não encontrado na base :/"
+        logger.warning(f"Erro ao buscar atleta '{atleta_id}', {error_msg}")
+        return {"mesage": error_msg}, 404
+
+    return jsonify({'atleta': atleta.toDict()})
+
+
+@atleta_routes.post('/atletas', responses={"200": AdicionarAtletasSchema})
+def adicionar_atleta(form: FormAddAtletaSchema):
+    """ Adiciona um atleta no banco de dados.
+
+    """
+    session = Session()
+    atleta = Atleta(request.form['nome'], request.form['sexo'],
+                    request.form['idade'], request.form['cidade'], request.form['categoria'])
+    atleta_obj = atleta.toDict()
     session.add(atleta)
+
     session.commit()
 
-    return jsonify({'message': 'Sucesso ao adicionar atleta.', atleta: jsonify(atleta.toDict())})
+    return jsonify({'atleta': atleta_obj})
+
+
+@atleta_routes.put('/atletas/<int:id>', responses={"200": AdicionarAtletasSchema})
+def editar_atleta(path: AtletaPath):
+    """ Edita um atleta existente no banco de dados.
+
+    """
+    atleta_id = path.id
+
+    session = Session()
+    atleta = session.query(Atleta).filter(Atleta.id == atleta_id).first()
+    if not atleta:
+        # se o atleta não foi encontrado
+        error_msg = "Atleta não encontrado na base :/"
+        logger.warning(f"Erro ao buscar atleta '{atleta_id}', {error_msg}")
+        return {"mesage": error_msg}, 404
+
+    atleta.nome = request.form['nome']
+    atleta.sexo = request.form['sexo']
+    atleta.idade = request.form['idade']
+    atleta.cidade = request.form['cidade']
+    atleta.categoria = request.form['categoria']
+
+    atleta_obj = {"nome": request.form["nome"], "sexo": request.form["sexo"], "idade": request.form["idade"],
+                  "cidade": request.form["cidade"], "categoria": request.form["categoria"]}
+
+    session.commit()
+
+    return jsonify({'atleta': atleta_obj})
+
+
+@atleta_routes.delete('/atletas/<int:id>', responses={"200": DeletarAtletaIdSchema})
+def deletar_atleta(path: AtletaPath):
+    """ Deleta um atleta no banco de dados.
+
+    """
+    atleta_id = path.id
+
+    session = Session()
+    # remove athlete from database
+    atleta = session.query(Atleta).filter(Atleta.id == atleta_id).first()
+
+    if not atleta:
+        # se o atleta não foi encontrado
+        error_msg = "Atleta não encontrado na base :/"
+        logger.warning(f"Erro ao deletar atleta '{atleta_id}', {error_msg}")
+        return {"mesage": error_msg}, 404
+
+    session.delete(atleta)
+    atleta_obj = atleta.toDict()
+    session.commit()
+
+    return jsonify({'atleta': atleta_obj})
--- routes/campeonato_routes.py
+++ routes/campeonato_routes.py
@@ -1,13 +1,23 @@
-from flask import Blueprint, jsonify, request
+from flask import jsonify, request
 from model import Session
 from model.campeonato import Campeonato
 from datetime import date
+from flask_openapi3 import Tag, APIBlueprint, Schema, Parameter
+from model.campeonato import Campeonato
+from schemas import ListarCampeonatosSchema, AdicionarCampeonatoSchema, FormAddCampeonatoSchema, ListarCampeonatoIdSchema, DeletarCampeonatoIdSchema, CampeonatoPath
 
-campeonato_routes = Blueprint('campeonato_routes', __name__)
+from logger import logger
 
-@campeonato_routes.route('/campeonatos', methods=['GET'])
+campeonatos_tag = Tag(name='Campeonatos', description='Operações relacionadas a campeonatos')
+
+campeonato_routes = APIBlueprint('campeonato_routes', __name__, abp_tags=[campeonatos_tag])
+
+
+@campeonato_routes.get('/campeonatos',  responses={"200": ListarCampeonatosSchema})
 def listar_campeonatos():
-    # listar campeonatos do banco
+    """ Retorna uma listagem de campeonatos cadastrados no banco de dados.
+
+    """
     session = Session()
     campeonatos = session.query(Campeonato).all()
     campeonatos_arr = []
@@ -18,17 +28,86 @@ def listar_campeonatos():
 
     return jsonify(campeonatos_arr)
 
-@campeonato_routes.route('/campeonatos', methods=['POST'])
+@campeonato_routes.post('/campeonatos', responses={"200": AdicionarCampeonatoSchema})
 def adicionar_campeonato():
-    session = Session()
+    """ Adiciona um campeonato no banco de dados.
 
-    print(request.form['data'])
-    # parse date from string
+    """    
+    session = Session()
+   
+    # parsear date da string
     data = date.fromisoformat(request.form['data'])
 
     campeonato = Campeonato(request.form['nome'], request.form['cidade'], data=data)
-
+    campeonato_obj = campeonato.toDict()
+    
     session.add(campeonato)
     session.commit()
 
-    return jsonify({'message': 'Sucesso ao adicionar campeonato.', })
+    return jsonify({'campeonato': campeonato_obj })
+
+@campeonato_routes.put('/campeonatos/<int:id>', responses={"200": AdicionarCampeonatoSchema})
+def editar_campeonato(path: CampeonatoPath):
+    """ Edita um campeonato existente no banco de dados.
+
+    """
+    campeonato_id = path.id
+    session = Session()
+    
+    # editar campeonato específico do banco:
+
+    campeonato = session.query(Campeonato).filter(Campeonato.id == campeonato_id).first()
+    if not campeonato:
+        error_msg = "Campeonato não encontrado na base :/"
+        logger.warning(f"Erro ao buscar campeonato '{campeonato_id}', {error_msg}")
+        return {"mesage": error_msg}, 404
+    campeonato.nome = request.form['nome']
+    campeonato.cidade = request.form['cidade']
+    data = date.fromisoformat(request.form['data'])
+    campeonato.data = data
+    session.commit()
+    
+    return jsonify({ 'campeonato': campeonato.toDict()})
+
+@campeonato_routes.get('/campeonatos/<int:id>',  responses={"200": ListarCampeonatoIdSchema})
+def listar_campeonatos_por_id(path: CampeonatoPath):
+    """ Retorna campeonato específico cadastrados no banco de dados, de acordo com o ID especificado.
+    """
+
+    # listar campeonato específico do banco:
+    campeonato_id = path.id
+
+    session = Session()
+    campeonato = session.query(Campeonato).filter(Campeonato.id == campeonato_id).first()
+
+    if not campeonato:
+        # se o campeonato não foi encontrado
+        error_msg = "Campeonato não encontrado na base :/"
+        logger.warning(f"Erro ao buscar campeonato '{campeonato_id}', {error_msg}")
+        return {"mesage": error_msg}, 404
+    
+    return jsonify({ 'campeonato': campeonato.toDict()})
+
+
+@campeonato_routes.delete('/campeonatos/<int:id>', responses={"200": DeletarCampeonatoIdSchema})
+def deletar_atleta(path: CampeonatoPath):
+    """ Deleta um campeonato no banco de dados.
+
+    """
+    campeonato_id = path.id
+
+    session = Session()
+    # remover campeonato do db
+    campeonato = session.query(Campeonato).filter(Campeonato.id == campeonato_id).first()
+
+    if not campeonato:
+        # se o campeonato não foi encontrado
+        error_msg = "Evento não encontrado na base :/"
+        logger.warning(f"Erro ao deletar campeonato '{campeonato_id}', {error_msg}")
+        return {"mesage": error_msg}, 404
+
+    session.delete(campeonato)
+    campeonato_obj = campeonato.toDict()
+    session.commit()
+
+    return jsonify({'campeonato': campeonato_obj})
