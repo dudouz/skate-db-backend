10f885b2725a03117007fdcb25a4850be5fbc329
Eduardo Iotti <ddz.iotti@gmail.com>
2023-04-30 12:58:45 -0300

    feat: instrucões openapi para rotas iniciais

--- app.py
+++ app.py
@@ -3,25 +3,26 @@ from flask import Flask, request, jsonify, redirect
 from urllib.parse import unquote
 from sqlalchemy.exc import IntegrityError
 from logger import logger
-from schemas import *
 
-app = OpenAPI(__name__)
+info = Info(title="Skate Database", version="1.0.0")
+app = OpenAPI(__name__, info=info)
 
 # definir tags da doc
-home_tag = Tag('Home', description='Documentação da API')
-atletas_tag = Tag('Atletas', description='Operações relacionadas a atletas')
-campeonatos_tag = Tag('Campeonatos', description='Operações relacionadas a campeonatos')
-resultados_tag = Tag('Resultados', description='Operações relacionadas a resultados')
+home_tag = Tag(name='Home', description='Documentação da Skate Database')
+campeonatos_tag = Tag(name='Campeonatos', description='Operações relacionadas a campeonatos')
+atletas_tag = Tag(name='Atletas', description='Operações relacionadas a atletas')
 
 # importar rotas
 from routes.atleta_routes import atleta_routes
 from routes.campeonato_routes import campeonato_routes
 
 # registrar rotas
-app.register_blueprint(atleta_routes)
-app.register_blueprint(campeonato_routes)
+app.register_api(atleta_routes)
+app.register_api(campeonato_routes)
 
-# declaa rota do open api!
-@app.route('/', tags=[home_tag])
-def hello_skaters():
+# declara rota do open api!
+@app.get('/', tags=[home_tag])
+def home():
+    """Redireciona para /openapi, tela que permite a escolha do estilo de documentação.
+    """
     return redirect('/openapi')
